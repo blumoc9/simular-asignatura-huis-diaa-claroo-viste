9418c463de9b667b60b23dbeeda89d7adfaa2ec3
skate.torres505@gmail.com <skate.torres505@gmail.com@6822833a-8619-4986-9f31-be7f7b2f4001>
2012-02-02 19:57:53 +0000

    Se Colocaron los nombres de los Integrantes

--- SistemaContadorInteractivo/src/Principal.java
+++ SistemaContadorInteractivo/src/Principal.java
@@ -1,4 +1,10 @@
+//Integrantes:
+// Santana, Adriana CI: 18.801.197
+// Paez, Maira CI: 19.618.874
+// Arteaga, Luis CI: 19.696.160
+// Colmenarez, Fernando CI: 18.923.926 
 import AgSistema.CSistema;
+
 
 
 public class Principal {
@@ -6,5 +12,5 @@ public class Principal {
 	public static void main(String[] args) {
 		new CSistema();
 	}
-
+	
 }
