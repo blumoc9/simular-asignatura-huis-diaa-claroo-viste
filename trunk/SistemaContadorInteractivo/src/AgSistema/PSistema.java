68fdbe4e917d74fec7dc57d483fda21d058f10ab
skate.torres505@gmail.com <skate.torres505@gmail.com@6822833a-8619-4986-9f31-be7f7b2f4001>
2012-02-02 19:57:53 +0000

    Se Colocaron los nombres de los Integrantes

--- trunk/SistemaContadorInteractivo/src/AgSistema/PSistema.java
+++ trunk/SistemaContadorInteractivo/src/AgSistema/PSistema.java
@@ -1,3 +1,8 @@
+//Integrantes:
+// Santana, Adriana CI: 18.801.197
+// Paez, Maira CI: 19.618.874
+// Arteaga, Luis CI: 19.696.160
+// Colmenarez, Fernando CI: 18.923.926 
 package AgSistema;
 import java.awt.BorderLayout;
 
